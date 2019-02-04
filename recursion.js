e6cfdfa611440c1ba2a127b90739f4b0221fca04
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-02-04 14:41:55 -0800

    recursion exercises, first pass, not completed yet

---
+++ recursion.js
@@ -0,0 +1,27 @@
+//Code begins here
+
+var factorial = function(num) {
+    var n = Number(num);
+
+    if (n===0) {
+        return 1;
+    } else {
+        return n*factorial(n-1);
+    }
+
+    return 1000;
+};
+
+
+
+
+
+   // var result;
+//return null;
+        
+    // while (n > 0) {
+    //     result = result * n;
+    //     n = n-1;
+    // }
+
+    //return result;
